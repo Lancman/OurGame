e13f16c7d7bd9d098351a2adcae38db0b46201b5
Lachin707 <tu7da4k8x@code.edu.az>
2024-02-06 02:15:51 +0400

    update by Lachin

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,25 +1,23 @@
-import './App.css'
+import "./App.css";
 import { PointerLockControls, Sky } from "@react-three/drei";
 import { Ground } from "./components/Ground";
-import {Physics} from "@react-three/rapier";
-import {Player} from "./components/Player";
-import {Cubes} from "./components/Cube.jsx";
+import { Physics } from "@react-three/rapier";
+import { Player } from "./components/Player";
+import { Cubes } from "./components/Cube.jsx";
 
 function App() {
-
-
-  return (
-    <>
-      <PointerLockControls />
-      <Sky sunPosition={[100, 20, 100]} />
-      <ambientLight intensity={1.5} />
-      <Physics gravity={[0, -20, 0]}>
-        <Ground />
-        <Player />
-        <Cubes />
-      </Physics>
-    </>
-  )
+	return (
+		<>
+			<PointerLockControls />
+			<Sky sunPosition={[100, 20, 100]} />
+			<ambientLight intensity={1.5} />
+			<Physics gravity={[0, -20, 0]}>
+				<Ground />
+				<Player />
+				<Cubes />
+			</Physics>
+		</>
+	);
 }
 
-export default App
+export default App;
